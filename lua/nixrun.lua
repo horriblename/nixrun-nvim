eb587929958ecd73eb94464f2ab683a7a5a49698
Ching Pei Yang <badnam3o.0@gmail.com>
2024-10-11 02:16:10 +0200

    doc: update nixrun.txt

--- lua/nixrun.lua
+++ lua/nixrun.lua
@@ -10,19 +10,19 @@ function M.setup(options)
 end
 
 ---@param installable string
----@param on_done fun(paths: string[])?
+---@param on_done fun(paths: string[])? Function to run on success. `paths` is the full path(s) to the installed packages
 function M.add_plugin(installable, on_done)
 	require('nixrun.lazy').includePlugin(installable, on_done)
 end
 
 ---@param installable string
----@param on_done fun(paths: string[])?
+---@param on_done fun(paths: string[])? Function to run on success. `paths` is the full path(s) to the installed packages
 function M.add_grammar(installable, on_done)
 	require('nixrun.lazy').includeGrammar(installable, on_done)
 end
 
 ---@param name string
----@param on_done fun()?
+---@param on_done fun()? Function to run on success.
 function M.add_lsp(name, on_done)
 	require('nixrun.lazy').setupLsp(name, on_done)
 end
