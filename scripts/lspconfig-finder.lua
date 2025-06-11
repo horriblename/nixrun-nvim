79f9ff4397a0bec1a80fea197cfd91f829a3f8f5
Ching Pei Yang <badnam3o.0@gmail.com>
2025-06-12 01:04:39 +0200

    script: use vim.lsp.config in finder

--- scripts/lspconfig-finder.lua
+++ scripts/lspconfig-finder.lua
@@ -1,10 +1,17 @@
 local function supported_servers()
-	return vim.iter(vim.fs.dir(vim.api.nvim__get_runtime({ 'lua/lspconfig/configs' }, false, {})[1]))
-		:map(function(fname) return string.gsub(fname, '.lua$', '') end)
+	return vim.iter(vim.api.nvim__get_runtime({ "lsp" }, true, {}))
+		:map(function(path)
+			return vim.iter(vim.fs.dir(path))
+				:map(function(name, type)
+					return type == "file" and name:match("(.*)%.lua$") or nil
+				end)
+				:totable()
+		end)
+		:flatten()
 end
 
 local function splitLine(str)
-	return string.gmatch(str, '[^\n]+')
+	return string.gmatch(str, "[^\n]+")
 end
 
 local function moduleExists(mod)
@@ -32,30 +39,37 @@ local function main()
 			return not skip
 		end)
 		:map(function(server_name)
-			local cmd = require('lspconfig')[server_name].config_def.default_config.cmd
-			if type(cmd) == 'table' then
+			local entry =
+				assert(vim.lsp.config[server_name], "server " .. server_name .. " is missing from vim.lsp.config")
+			local cmd = entry.cmd
+			if type(cmd) == "table" then
+				if cmd[1] == nil then
+					print("skipped " .. server_name .. " due to empty array cmd")
+					return nil
+				end
 				return cmd[1], server_name
 			else
 				-- godot lsp config uses vim.lsp.rpc.connect (returns a function) for some reason
 				return nil
 			end
 		end)
-		:map(function(cmd, server_name)
-			local nix_locate = io.popen('nix-locate --whole-name --minimal bin/' .. cmd, 'r')
-			if nix_locate then
-				local pkgs = nix_locate:read('*a')
-				nix_locate:close()
-				return server_name, vim.iter(splitLine(pkgs)):totable()
-			end
-		end)
 
 	local outfile = assert(io.open('gen.txt', 'w'))
 
-	for server_name, pkg in commands do
-		if #pkg == 1 then
+	for cmd, server_name in commands do
+		local pkg
+		local nix_locate = io.popen('nix-locate --whole-name --minimal bin/' .. cmd, 'r')
+		if nix_locate then
+			local pkgs = nix_locate:read('*a')
+			nix_locate:close()
+			pkg = vim.iter(splitLine(pkgs)):totable()
+		end
+
+		if pkg == nil then
+		elseif #pkg == 1 then
 			single_pkg = single_pkg + 1
 			local f = assert(io.open('lua/nixrun/lsp/' .. server_name .. '.lua', 'w'))
-			f:write(binding_template:format(pkg))
+			f:write(binding_template:format(pkg[1]))
 			f:close()
 		elseif #pkg == 0 then
 			no_pkg = no_pkg + 1
